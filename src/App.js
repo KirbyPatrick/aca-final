406715cd63246c1fcc4a7b1abaee587db9d3e545
KirbyPatrick <pkirbyinatx@gmail.com>
2019-01-08 04:55:51 -0600

    added fetch call to API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import Titles from "./components/Titles"
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather"
+
+const API_KEY = "ea98074eccba05ca87fcda45a93139a7"
+// const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=Austin,us&appid=${API_KEY}&units=metric`;
+// const savedURL = `https://api.openweathermap.org/data/2.5/weather?q=Austin,us&appid=ea98074eccba05ca87fcda45a93139a7`
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Austin,us&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
+
+
   render() {
     return (
       <div>
-        <p>Hello!</p>
-        <h1>YO!</h1>
-      </div>
+        <Titles/>
+        <Form getWeather={this.getWeather}/>
+        <Weather/>
+      </div> 
     );
   }
 }
